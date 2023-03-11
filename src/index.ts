40699e390fd14b84df9eb19a3e1cd41c0f3cc85e
pengchongfu <pengchongfu@gmail.com>
2023-03-11 13:32:23 +0800

    send weixin message

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
+import { XMLParser } from "fast-xml-parser";
+
 export interface Env {
   KV: KVNamespace;
   WEIXIN_APP_ID: string;
   WEIXIN_SECRET: string;
 }
+
+type WeixinMessage = {
+  FromUserName: string;
+  MsgType:
+    | "text"
+    | "image"
+    | "voice"
+    | "video"
+    | "shortvideo"
+    | "location"
+    | "link";
+  Content: string;
+};
 
 type WeixinAccessTokenResponse = {
   access_token: string;
@@ -18,13 +33,49 @@ const getWeixinAccessToken = async (env: Env) => {
   return access_token;
 };
 
+const sendWeixinMessage = async (
+  env: Env,
+  weixinMessage: WeixinMessage,
+  content: string
+) => {
+  const weixinAccessToken = await env.KV.get(WEIXIN_ACCESS_TOKEN_KEY);
+  await fetch(
+    `https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=${weixinAccessToken}`,
+    {
+      body: JSON.stringify({
+        touser: weixinMessage.FromUserName,
+        msgtype: "text",
+        text: {
+          content,
+        },
+      }),
+      method: "POST",
+      headers: {
+        "content-type": "application/json;charset=UTF-8",
+      },
+    }
+  );
+};
+
+const replyUser = async (env: Env, weixinMessage: WeixinMessage) => {
+  if (weixinMessage.MsgType !== "text") {
+    await sendWeixinMessage(env, weixinMessage, "非常抱歉，目前仅支持文本消息");
+    return;
+  }
+};
+
 export default {
   async fetch(
     request: Request,
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    return new Response("Hello World!");
+    const parser = new XMLParser();
+    const weixinMessage: WeixinMessage = parser.parse(await request.text());
+
+    ctx.waitUntil(replyUser(env, weixinMessage));
+
+    return new Response();
   },
   async scheduled(
     controller: ScheduledController,
